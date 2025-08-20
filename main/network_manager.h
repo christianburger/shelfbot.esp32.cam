bf832b7f0fad9fa45612fe0b6ab42502065a5462
christian_burger <christian_burger@yahoo.com>
2025-08-19 21:39:23 -0300

    fix: Define missing event bit for micro-ROS status

--- main/network_manager.h
+++ main/network_manager.h
@@ -9,6 +9,7 @@
 extern EventGroupHandle_t s_wifi_event_group;
 #define WIFI_CONNECTED_BIT BIT0
 #define MDNS_STARTED_BIT   BIT1
+#define UROS_INIT_COMPLETE_BIT BIT2
 
 #define WIFI_SSID "dlink-30C0"
 #define WIFI_PASS "ypics98298"
